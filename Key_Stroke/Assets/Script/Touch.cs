8e122a82dcd5d419d827fa19b770b031a278eb53
hiratake <hiratake@hiratakenoMacBook-Air.local>
2017-09-27 17:53:14 +0900

    鍵盤に対応して拡大を1つだけ対応

--- Key_Stroke/Assets/Script/Touch.cs
+++ Key_Stroke/Assets/Script/Touch.cs
@@ -5,43 +5,53 @@ using UnityEngine;
 
 public class Touch : MonoBehaviour {
 
+	private bool flag;
+	private string gameobject_name;
 	// Use this for initialization
 	void Start () {
-		
+		flag = false;
 	}
 	
 	void Update()
 	{
 		
 		OnTouchDown();
+		if(flag == true){
+		ChangeScale ();
+		}
 	}
 
 	//スマホ向け そのオブジェクトがタッチされていたらtrue（マルチタップ対応）
 	void OnTouchDown() {
 		// タッチされているとき
-		if( 0 < Input.touchCount){
+		if (0 < Input.touchCount) {
 			// タッチされている指の数だけ処理
-			for(int i = 0; i < Input.touchCount; i++){
-				UnityEngine.Touch t = Input.GetTouch(i); // タッチ情報をコピー
-				if(t.phase == TouchPhase.Began ){ // タッチしたときかどうか
+			for (int i = 0; i < Input.touchCount; i++) {
+				UnityEngine.Touch t = Input.GetTouch (i); // タッチ情報をコピー
+				if (t.phase == TouchPhase.Began) { // タッチしたときかどうか
+					flag = true;
 					//タッチした位置からRayを飛ばす
-					Ray ray = Camera.main.ScreenPointToRay(t.position);
-					RaycastHit hit = new RaycastHit();
-					if (Physics.Raycast(ray, out hit)){
+					Ray ray = Camera.main.ScreenPointToRay (t.position);
+					RaycastHit hit = new RaycastHit ();
+					if (Physics.Raycast (ray, out hit)) {
 						//Rayを飛ばしてあたったオブジェクトが自分自身だったら
-						if (hit.collider.gameObject != null){
-							Debug.Log( hit.collider.gameObject.name);
+						if (hit.collider.gameObject != null) {
+							Debug.Log (hit.collider.gameObject.name);
+							gameobject_name = hit.collider.gameObject.name;
 						}
 					}
 
-					ChangeScale ();
 				}
 			}
+		} else {
+			flag = false;
 		}
 	}
 
 	void ChangeScale(){
-		GameObject a = GameObject.Find("a");
-		a.transform.localScale = new Vector3(0.1f + 0.1f*Time.deltaTime, 0.1f, 0.1f);
+		if(gameobject_name.Equals("Cube_0")){
+		GameObject magic_0= GameObject.Find("magic_0");
+		magic_0.transform.localScale += new Vector3(0.001f, 0.001f, 0.0f);
+		}
 	}
 }
